a493272614c606942e5b06dfa97802907942a972
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2022-09-18 21:32:34 +0100

    Delete src directory

--- src/constants/assets.js
+++
@@ -1,8 +0,0 @@
-// This file holds configurations for assets files
-
-const avatar = './assets/images/fake_passport.png';
-const image_placeholder = './assets/images/img_placeholder.png';//public\assets\images\img_placeholder.png
-const school_logo_placeholder =  './assets/images/school_logo_placeholder.png';
-const brand_logo = './assets/logo.png';
-
-export { brand_logo, avatar, image_placeholder, school_logo_placeholder };
--- src/constants/statuses.js
+++
@@ -1,7 +0,0 @@
-export const statuses = {
-    idle:'idle',
-    loading:'loading',
-    loaded: 'loaded',
-    done: 'done',
-    failed: 'failed'
-}
